643fe07cd795343e1580c45cb1cf07726acce4b0
ephraim <ephraims.818@gmail.com>
2026-01-15 18:24:12 +0600

    JS-EXERCISES - W3R -- 02. Check Blank String

---
+++ 01. Strings/02. Check Blank String/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* ================= SOLUTION ================= */
+function is_Blank(str) {
+    return str.length === 0;
+}
+
+console.log(is_Blank("")); // true
+console.log(is_Blank("abc")); // false
